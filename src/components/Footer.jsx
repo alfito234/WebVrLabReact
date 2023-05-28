b59d14e4f19f5604c49be9b20466a0c3525e5216
alfito234 <alfito.devaga@gmail.com>
2023-05-28 17:55:57 +0700

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,14 +3,24 @@ import styles from "../style";
 
 import Logo from "../assets/Image/logo-vr.png";
 import { footerLinks, socialMedia } from "../constants";
+import { facebook, twitter } from "../assets";
 
 const Footer = () => (
-  <section className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
-    <div className={`${styles.flexStart} md:flex-row flex-col items-center mb-8 w-full`}>
+  <section className={`w-full items-center ${styles.paddingY} flex-col`}>
+    <div className={"flex flex-row justify-between items-center mb-8 w-full"}>
       <div>
-        <img src={Logo} alt="VR Lab" className="w-[100px] h-[100px] object-contain"/>
+        <img
+          src={Logo}
+          alt="VR Lab"
+          className="w-[100px] h-[100px] object-contain"
+        />
       </div>
-      <h1>Hello</h1>
+
+      <div className="flex flex-row items-center space-x-6">
+        <h1 className="font-poppins text-white">Privacy& Policy</h1>
+        <img src={facebook} className="w-[30px] h-[30px] object-contain" />
+        <img src={twitter} className="w-[30px] h-[30px] object-contain" />
+      </div>
     </div>
   </section>
 );
